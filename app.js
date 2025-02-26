f90b61e1d7456f661b3bbbf13c277645021d6209
= <ricksarkar2005@gmail.com>
2025-02-26 20:42:10 +0530

    nodeJs init

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { configDotenv } from 'dotenv';
+import fs from 'fs';
+import path from 'path';
+configDotenv();
+
+const app = express();
+
+app.use(express.static(path.join(process.cwd(), '/public')));
+
+const home = async ( req , res , next ) => {
+    const html = fs.readFileSync(path.join(process.cwd(), '/index.html'), 'utf-8');
+    return res.status(200).send(html);
+};
+
+app.get('/' , home);
+app.get("*" , home);
+
+app.listen(80, () => console.log('Server is running on port 80'));
